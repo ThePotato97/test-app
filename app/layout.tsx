8495c917ce448dfcdc2dcabaf7f7417dab5ade12
ThePotato97 <10260415+ThePotato97@users.noreply.github.com>
2023-06-08 15:18:13 +0100

    add logging in

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+'use client'
+import { AuthContextProvider } from './context/AuthContext'
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <AuthContextProvider>
+          {children}
+        </AuthContextProvider>
+      </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,74 @@
-import 'server-only';
-import { notFound } from 'next/navigation';
-import * as admin from 'firebase-admin';
+'use client'
+import React, { useContext } from "react";
+import signIn from "./firebase/auth/signin";
+import { useRouter } from 'next/navigation'
+import signUp from "./firebase/auth/signup";
+import signOut from "./firebase/auth/signout";
+import { AuthContext } from "./context/AuthContext";
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-    })
-  });
+function Page() {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const router = useRouter()
+    const { user } = useContext(AuthContext)
+    const handleSignup = async (event) => {
+        event.preventDefault()
+
+        const { result, error } = await signUp(email, password);
+
+        if (error) {
+            return console.log(error)
+        }
+
+        // else successful
+        console.log(result)
+    }
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        const { result, error } = await signIn(email, password);
+
+        if (error) {
+            return console.log(error)
+        }
+
+        // else successful
+        console.log(result)
+
+    }
+    const handleLogout = async (event) => {
+        event.preventDefault()
+
+        const { result, error } = await signOut();
+
+        if (error) {
+            return console.log(error)
+        }
+
+        // else successful
+        console.log(result)
+    }
+    return (<div className="wrapper">
+        <div>Your email is {user?.email ? user.email : 'Not signed in'}</div>
+        <div>Your uid is {user?.uid ? user.uid : 'Not signed in'}</div>
+        <div className="form-wrapper">
+            <h1 className="mt-60 mb-30">Sign up</h1>
+            <form onSubmit={handleSignup} className="form">
+                <label htmlFor="email">
+                    <p>Email</p>
+                    <input onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" />
+                </label>
+                <label htmlFor="password">
+                    <p>Password</p>
+                    <input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" />
+                </label>
+                <button type="submit">Sign up</button>
+                <button onClick={handleLogin} type="submit">Login</button>
+                <button onClick={handleLogout} type="submit">Sign out</button>
+            </form>
+        </div>
+
+    </div>);
 }
 
-const db = admin.firestore();
-
-export default async function Page() {
-  const user = await db.collection('users').doc('leerob').get();
-
-  if (!user.exists) {
-    notFound();
-  }
-
-  return <div>Hello, {user.data().name}!</div>;
-}
+export default Page;
